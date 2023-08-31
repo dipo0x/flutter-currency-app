0055b428679284fcf7b5be0828ad1f6a82bb5810
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2023-08-31 07:24:03 +0100

    feat: completed input field

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,43 +5,52 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override 
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(
+        10
+      )
+    );
+    return Scaffold(
       backgroundColor: Colors.blueGrey, 
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '0', 
-              style: TextStyle(
-                fontSize: 55,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255)
-              ),
-            ),
-            TextField(
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255)
-              ),
-              decoration: InputDecoration(
-                hintText: 'Please enter the amount in USD',
-                hintStyle: TextStyle(
-                  color: Color.fromARGB(153, 0, 0, 0)
+             const Text(
+               '0', 
+               style: TextStyle(
+                 fontSize: 55,
+                 fontWeight: FontWeight.bold,
+                 color: Color.fromARGB(255, 255, 255, 255)
+               )
+             ),
+           Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255)
                 ),
-                prefixIcon: Icon(Icons.monetization_on),
-                filled: true,
-                prefixIconColor: Color.fromARGB(153, 0, 0, 0),
-                fillColor: Colors.white,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 2.0,
-                    style: Borderstyle.solid,
-                    color: Colors.black,
-
-                  )
-                )
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount in USD',
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(153, 0, 0, 0)
+                  ),
+                  prefixIcon: const Icon(Icons.monetization_on),
+                  filled: true,
+                  prefixIconColor: const Color.fromARGB(153, 0, 0, 0),
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true
+                ),
               ),
-            )
+            ) ,
           ],
         ), 
       ),
